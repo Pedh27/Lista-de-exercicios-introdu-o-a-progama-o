73cae277accc64570398fb2dddd0186475792988
Pedro Henrique 1Ds <sousa.pedro27@escola.pr.gov.br>
2026-09-24 15:00:56 +0000

    execicios de kotlin

---
+++ Exercicios KOTLIN/ex02.kt
@@ -0,0 +1,12 @@
+fun main (){
+    val nome = "Pedro"
+    val idade = 15
+    val cidade = "Paranaguá"
+    val linguagemFavorita = "python"
+
+    println("Apresentação pessoal")
+    println("Nome: $nome")
+    println("Idade: $idade anos")
+    println("Cidade: $cidade")
+    println("Linguagem Favorita: $linguagemFavorita")
+}
